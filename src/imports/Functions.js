f6e1d3bdf20db209cf766aa076b4821e19f69f2e
sammckay10 <samuel@alt-design.net>
2017-02-02 12:06:32 +0000

    Added in fix for when viewing pages in preview mode, a second request is sent for th page by it's ID. Also added in getUrlParams method.

--- src/imports/Functions.js
+++ src/imports/Functions.js
@@ -2,11 +2,27 @@ import store from '../vuex/store';
 
 export default class Functions {
 
-    static updateAdminBar() {
+  static updateAdminBar() {
 
-        let editPage = document.getElementById('wp-admin-bar-edit').querySelector('a');
-        if (editPage) editPage.setAttribute('href', store.state.adminUrl + '/post.php?post=' + store.state.page.ID + '&action=edit')
+    let editPage = document.getElementById('wp-admin-bar-edit').querySelector('a');
+    if (editPage) editPage.setAttribute('href', store.state.adminUrl + '/post.php?post=' + store.state.page.ID + '&action=edit');
 
+  }
+
+  static getUrlParams() {
+    let queryString = window.location.href.split('?')[1];
+    if (queryString) {
+      let keyValuePairs = queryString.split('&');
+      let keyValue, params = {};
+
+      keyValuePairs.forEach(pair => {
+        keyValue = pair.split('=');
+        params[keyValue[0]] = decodeURIComponent(keyValue[1]).replace('+', ' ');
+      });
+
+      return params;
     }
+    return false;
+  }
 
-}
+};
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,31 +1,43 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import jump from 'jump.js';
 import routes from './Routes';
 import store from '../vuex/store';
-import jump from 'jump.js';
+import Functions from '../imports/Functions';
 
 Vue.use(VueRouter);
 
 // Main Router Instance
 const router = new VueRouter({
-    routes,
-    mode: 'history'
+  routes,
+  mode: 'history'
 });
 
 // Run before each route change
 router.beforeEach((to, from, next) => {
 
-    jump('html', {
-        duration: window.pageYOffset > 10 ? 500 : 0,
-        callback(){
-            store.commit('updatePage', (to.path ? to.path : 'home'));
-            next();
-        }
-    });
+  jump('html', {
+    duration: window.pageYOffset > 10 ? 500 : 0,
+    callback(){
+      new Promise(resolve => {
+        store.commit('updatePage', (to.fullPath ? to.fullPath : 'home'));
+        resolve();
+      }).then(() => {
+        /*
+         * Check if the URL we're visiting is a preview and then get the appropriate data by a new request.
+         * The queryAll endpoint kills the current request if it's a preview
+         * */
+        if (to.fullPath.indexOf('preview=true') > -1) store.commit('updatePageById', (Functions.getUrlParams().p));
+      });
+
+      next();
+    }
+  });
 });
+
 
 // Run after each route change
 // router.afterEach((to, from) => {
 // });
 
-export default router
+export default router;
